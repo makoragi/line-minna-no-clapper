2aa59c9b35e90be1f73fe255a71bc947f040c7b6
Makoto Sakuragi <m-sakuragi@pbc-west.co.jp>
2018-09-12 16:53:34 +0900

    デバッグ

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const PORT = process.env.PORT || 5000;
 const LINE_CHANNEL_TOKEN = process.env.LINE_CHANNEL_TOKEN;
 const LINE_CHANNEL_SECRET = process.env.LINE_CHANNEL_SECRET;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const config = {
   channelAccessToken: LINE_CHANNEL_TOKEN,
